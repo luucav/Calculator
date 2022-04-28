e106757da7794974ef1eeac5b56439522fd3aeeb
luucav <80016546+luucav@users.noreply.github.com>
2022-04-28 10:56:25 -0300

    encontrando erro

--- script.js
+++ script.js
@@ -11,38 +11,37 @@ window.onload = () => {
     input.value = "";
 };
 
-//Acess each class using foEach
+//Access each class using forEach
 button_input.forEach((button_class) => {
     button_class.addEventListener("click", () => {
-        if(equal_pressed == 1) {
-            input.value = "";
-            equal_pressed = 0;
-        }
-        //Display value of each button
-        input.value += button_class.value;
+      if (equal_pressed == 1) {
+        input.value = "";
+        equal_pressed = 0;
+      }
+      //display value of each button
+      input.value += button_class.value;
     });
 });
 
-//Solve the user's input when clicked on equal sing
+//Solve the user's input when clicked on equal sign
 equal.addEventListener("click", () => {
     equal_pressed = 1;
     let inp_val = input.value;
-  try {
-    //evaluate user's input
-    let solution = eval(inp_val);
-    //True for natural numbers
-    //false for decimals
-    if (Number.isInteger(solution)) {
-      input.value = solution;
-    } else {
-      input.value = solution.toFixed(2);
+    try {
+      //evaluate user's input
+      let solution = eval(inp_val);
+      //True for natural numbers
+      //false for decimals
+      if (Number.isInteger(solution)) {
+        input.value = solution;
+      } else {
+        input.value = solution.toFixed(2);
+      }
+    } catch (err) {
+      //If user has entered invalid input
+      alert("Invalid Input");
     }
-  } catch (err) {
-    //If user has entered invalid input
-    alert("Invalid Input");
-  }
-
-});
+  });
 
 //Clear Whole Input
 clear.addEventListener("click", () => {
